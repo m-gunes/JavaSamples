000c230568c4b33be1381b3ba6476d26b8a905e3
m-gunes <mistafagunes@gmail.com>
2024-08-29 11:49:43 +0300

    bubble sort test

--- src/org/csystem/util/array/test/ArrayUtilBubbleSortTest.java
+++ src/org/csystem/util/array/test/ArrayUtilBubbleSortTest.java
@@ -1,18 +1,30 @@
 package org.csystem.util.array.test;
 
-import org.csystem.util.array.ArrayUtil;
-import org.csystem.util.array.ArrayUtil.*;
-
 import java.util.Random;
+import java.util.Scanner;
+
+import static org.csystem.util.array.ArrayUtil.*;
 
 public class ArrayUtilBubbleSortTest {
     public static void run()
     {
-        int [] a = ArrayUtil.generateRandomArray(new Random(), 10, 1, 100);
-        ArrayUtil.print(a);
-        System.out.println("Sorting.....");
-        ArrayUtil.bubbleSort(a, true);
-        ArrayUtil.print(a);
+        Scanner kb = new Scanner(System.in);
+        Random r = new Random();
+        while (true){
+            System.out.print("Input a number:");
+            int count = Integer.parseInt(kb.nextLine());
+
+            if(count <= 0)
+                break;
+
+            int [] a = generateRandomArray(r, count, 0, 100);
+            print(a, 2);
+            boolean descending = r.nextBoolean();
+
+            System.out.printf("Array is sorted %s:%n", descending ? "descending" : "ascending");
+            bubbleSort(a, descending);
+            print(a, 2);
+        }
     }
     public static void main(String [] args)
     {
