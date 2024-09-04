a8e5aa11867d395909f983cf2b6be1b887e23dd8
m-gunes <mistafagunes@gmail.com>
2024-09-04 15:18:40 +0200

    update selection sort

--- src/org/csystem/util/array/ArrayUtil.java
+++ src/org/csystem/util/array/ArrayUtil.java
@@ -1,8 +1,3 @@
-/**
- * Utility class for array operations
- * Last Update: 22nd August 2024
- * @author Java-Jan-2024 Group
- */
 package org.csystem.util.array;
 
 import java.util.Arrays;
@@ -10,62 +5,12 @@ import java.util.Random;
 
 public class ArrayUtil {
 
-    public static void selectionSortAscending(int [] a)
-    {
-        for(int i = 0; i < a.length - 1; ++i) {
-           int min = i;
-
-           for(int k = i + 1; k < a.length; ++k)
-               if(a[k] < a[min])
-                   min = k;
-
-           if(i != min)
-               swap(a, i, min);
-        }
-    }
-    public static void selectionSortDescending(int [] a)
-    {
-        for(int i = 0; i < a.length - 1; ++i) {
-            int max = i;
-
-            for(int k = i + 1; k < a.length; ++k)
-                if(a[k] > a[max])
-                    max = k;
-
-            if(i != max)
-                swap(a, i, max);
-        }
-    }
-    public static void selectionSort(int [] a)
-    {
-        selectionSort(a, false);
-    }
-    public static void selectionSort(int [] a, boolean descending)
-    {
-        if(descending)
-            selectionSortDescending(a);
-        else
-            selectionSortAscending(a);
-    }
-
-
-    public static void swap(int [] a, int i, int k)
-    {
-        int temp = a[i];
-        a[i] = a[k];
-        a[k] = temp;
-    }
     public static void bubbleSortAscending(int [] a)
     {
         for(int i = 0; i < a.length - 1; ++i)
             for(int k = 0; k < a.length - 1 - i; ++k)
                 if(a[k] > a[k + 1])
                     swap(a, k, k+1);
-
-//            for(int k = 1; k < a.length - i; ++k)
-//                if(a[k-1] > a[k])
-//                    swap(a, k, k-1);
-
     }
 
     public static void bubbleSortDescending(int [] a)
@@ -87,6 +32,61 @@ public class ArrayUtil {
             bubbleSortDescending(a);
         else
             bubbleSortAscending(a);
+    }
+
+    public static void selectionSortAscending(int [] a)
+    {
+        // Seçerek sıralama algoritmasında en küçük eleman bulunur, ilk eleman ile değiştirilir. Dizi bir daraltılır, aynı işlem daraltılmış dizi için yapılır.
+        int min, minIndex; // swap islemini kolaylastirmak icin tanimlandi
+        for(int i = 0; i < a.length - 1; ++i) {
+           min = a[i];
+           minIndex = i;
+
+           for(int k = i + 1; k < a.length; ++k)
+               if(a[k] < min) {
+                   min = a[k];
+                   minIndex = k;
+               }
+           a[minIndex] = a[i];
+           a[i] = min;
+        }
+    }
+
+    public static void selectionSortDescending(int [] a)
+    {
+        int max, maxIndex;
+        for(int i = 0; i < a.length - 1; ++i) {
+            max = a[i];
+            maxIndex = i;
+
+            for(int k = i + 1; k < a.length; ++k)
+                if(max < a[k]) {
+                    max = a[k];
+                    maxIndex = k;
+                }
+            a[maxIndex] = a[i];
+            a[i] = max;
+        }
+    }
+
+    public static void selectionSort(int [] a)
+    {
+        selectionSort(a, false);
+    }
+    public static void selectionSort(int [] a, boolean descending)
+    {
+        if(descending)
+            selectionSortDescending(a);
+        else
+            selectionSortAscending(a);
+    }
+
+
+    public static void swap(int [] a, int i, int k)
+    {
+        int temp = a[i];
+        a[i] = a[k];
+        a[k] = temp;
     }
     public static int [] generateRandomArray(Random random, int count, int origin, int bound)
     {
