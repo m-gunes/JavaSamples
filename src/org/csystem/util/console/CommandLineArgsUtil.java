0311852cbc90a58f5f0b62907dea694e0e280f1f
m-gunes <mistafagunes@gmail.com>
2024-09-18 17:15:29 +0200

    Create CommandLineArgsUtil.java

---
+++ src/org/csystem/util/console/CommandLineArgsUtil.java
@@ -0,0 +1,29 @@
+package org.csystem.util.console;
+
+public class CommandLineArgsUtil {
+    public static void checkLengthEquals(int len, int argsLen, String message)
+    {
+        checkLengthEquals(len, argsLen, message, 1);
+    }
+
+    public static void checkLengthEquals(int len, int argsLen, String message, int exitCode)
+    {
+        if(len != argsLen) {
+            System.err.println(message);
+            System.exit(exitCode);
+        }
+    }
+
+    public static void checkLengthGreater(int len, int argsLen, String message)
+    {
+        checkLengthGreater(len, argsLen, message, 1);
+    }
+
+    public static void checkLengthGreater(int len, int argsLen, String message, int exitCode)
+    {
+        if(len <= argsLen) {
+            System.err.println(message);
+            System.exit(exitCode);
+        }
+    }
+}
