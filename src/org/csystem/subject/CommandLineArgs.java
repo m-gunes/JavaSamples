65a3de30e66f2556327703d1373e4c6a76f67986
m-gunes <mistafagunes@gmail.com>
2024-09-19 10:16:04 +0200

    Create CommandLineArgs.java

---
+++ src/org/csystem/subject/CommandLineArgs.java
@@ -0,0 +1,22 @@
+package org.csystem.subject;
+
+import java.util.Random;
+
+import static org.csystem.util.console.CommandLineArgsUtil.checkLengthEquals;
+
+public class CommandLineArgs {
+
+    public static void main(String[] args) {
+
+        checkLengthEquals(3, args.length, "usage: java org.csystem.app.App <count> <origin> <bound>" );
+
+        Random random = new Random();
+        int count = Integer.parseInt(args[0]);
+        int origin = Integer.parseInt(args[1]);
+        int bound = Integer.parseInt(args[2]);
+
+        for(int i = 0; i < count; ++i)
+            System.out.printf("%d ", random.nextInt(origin, bound));
+
+    }
+}
