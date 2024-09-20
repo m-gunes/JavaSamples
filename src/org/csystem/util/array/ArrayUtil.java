a7c72f79baaa09f5031c7133318a6670a9bfdd40
m-gunes <mistafagunes@gmail.com>
2024-09-20 16:56:17 +0200

    Update ArrayUtil.java

--- src/org/csystem/util/array/ArrayUtil.java
+++ src/org/csystem/util/array/ArrayUtil.java
@@ -231,18 +231,42 @@ public class ArrayUtil {
 
     public static int max(int [] a) {
         int max = a[0];
+
         for(int i = 1; i < a.length; ++i)
             max = Math.max(max, a[i]);
-//            if(max < a[i])
-//                max = a[i];
+
         return max;
     }
+
+    public static int max(int [][] a)
+    {
+        int result = Integer.MIN_VALUE; // Don't forget
+
+        for(int [] array : a )
+            result = Math.max(result, max(array));
+
+        return result;
+    }
+
     public static int min(int [] a) {
         int min = a[0];
+
         for(int i = 1; i < a.length; ++i)
-            if(a[i] < min)
-                min = a[i];
+            min = Math.min(min, a[i]);
+
         return min;
     }
+
+    public static int min(int [][] a)
+    {
+        int result = Integer.MAX_VALUE; // Don't forget
+
+        for(int [] array : a)
+            result = Math.min(result, min(array));
+
+        return result;
+    }
+
+
     //...
 }
