7979bf7ca92caacc482a1472b1ed35e55cac7e14
m-gunes <mistafagunes@gmail.com>
2024-11-10 19:21:27 +0100

    updete point class

---
+++ src/org/csystem/math/geometry/PointCommon.java
@@ -0,0 +1,16 @@
+package org.csystem.math.geometry;
+
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+class PointCommon {
+    static String toString(double x, double y)
+    {
+        return "(%f, %f)".formatted(x, y);
+    }
+
+    static double euclideanDistance(double x1, double y1, double x2, double y2)
+    {
+        return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+    }
+}
