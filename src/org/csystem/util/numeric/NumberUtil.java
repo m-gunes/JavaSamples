a136d7c78976aecdcdee645ec2623af4e038397d
m-gunes <mistafagunes@gmail.com>
2024-11-11 15:04:46 +0100

    Update NumberUtil.java
    create numToStr3DigitsTR, getDigitsInThrees, numToStrTR methods

--- src/org/csystem/util/numeric/NumberUtil.java
+++ src/org/csystem/util/numeric/NumberUtil.java
@@ -5,9 +5,36 @@
  */
 package org.csystem.util.numeric;
 
+
 public class NumberUtil {
     private NumberUtil()
     {
+    }
+    private static final String [] ONES_TR = {"", "bir", "iki", "üç", "dört", "beş", "altı", "yedi", "sekiz", "dokuz"};
+    private static final String [] TENS_TR = {"", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan"};
+    private static final String [] NUMBER_UNITS_TR = {"kentilyon", "katrilyon", "trilyon", "milyar", "milyon", "bin", ""};
+
+    private static String numToStr3DigitsTR(int val)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        int a = val / 100; // yuzler basamagi
+        int b = val / 10 % 10; // onlar basamagi
+        int c = val % 10; // birler basamadi
+
+        if (a != 0) {
+            if(a != 1)
+                sb.append(ONES_TR[a]).append(" ");
+            sb.append("yüz ");
+        }
+
+        if (b != 0)
+            sb.append(TENS_TR[b]).append(" ");
+
+        if (c != 0)
+            sb.append(ONES_TR[c]).append(" ");
+
+        return sb.substring(0, sb.length() - 1);
     }
 
     public static int countDigits(long a)
@@ -62,6 +89,17 @@ public class NumberUtil {
             --i;
         }
         return digits;
+    }
+
+    public static int [] getDigitsInThrees(long a)
+    {
+        a = Math.abs(a);
+        int [] threes = new int[a == 0 ? 1 : (int) (Math.log10(a) / 3) + 1]; // Check the operator precedence table.
+
+        for (int i = threes.length - 1; a != 0; threes[i--] = (int) (a % 1000), a /= 1000)
+            ;
+
+        return threes;
     }
 
     public static int getDigitsPowSum(int a)
@@ -154,6 +192,20 @@ public class NumberUtil {
                 result = i;
             }
         return result;
+    }
+
+    public static String numToStrTR(long a)
+    {
+        if (a == 0)
+            return "Sifir";
+
+        int [] threes = getDigitsInThrees(a);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < threes.length; ++i)
+            sb.append(numToStr3DigitsTR(threes[i])).append(" ").append(NUMBER_UNITS_TR[(NUMBER_UNITS_TR.length - threes.length) + i]).append(" ");
+
+        return sb.substring(0, sb.length() - 1);
     }
 
     public static int reverse(int val)
