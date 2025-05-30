62abd97b58cfd7616d69c9532b3f6e483bd2b44e
m-gunes <mistafagunes@gmail.com>
2025-05-30 11:48:11 +0200

    switch pattern (pattern matching of switch)

--- src/org/csystem/app/generator/DemoObjectArrayGeneratorApp.java
+++ src/org/csystem/app/generator/DemoObjectArrayGeneratorApp.java
@@ -21,23 +21,38 @@ public class DemoObjectArrayGeneratorApp {
             System.out.println("-------------------------------------------------------------------------------------");
             System.out.printf("Dynamic type:%s%n", o.getClass().getName());
 
-            if (o instanceof String s) {
-                String upper = s.toUpperCase();
-                System.out.printf("Text:%s, Upper:%s%n", s, upper);
+            switch (o) {
+                case String s -> {
+                    String upper = s.toUpperCase();
+                    System.out.printf("Text:%s, Upper:%s%n", s, upper);
+                }
+                case IntValue iVal -> {
+                    int val = iVal.getValue();
+                    System.out.printf("%d * %d = %d%n", val, val, val * val);
+                }
+                case Point p -> System.out.printf("Distance to origin:%f%n", p.euclideanDistance());
+                case Circle c -> System.out.printf("Radius:%f, Area:%f, Circumference:%f%n", c.getRadius(), c.getArea(), c.getCircumference());
+                case Complex c -> System.out.printf("||%s|| = %f%n", c.toString(), c.getLength());
+                default -> {}
             }
-            else if (o instanceof IntValue iVal) {
-                int val = iVal.getValue();
-                System.out.printf("%d * %d = %d%n", val, val, val * val);
-            }
-            else if (o instanceof Point p) {
-                System.out.printf("Distance to origin:%f%n", p.euclideanDistance());
-            }
-            else if (o instanceof Circle c) {
-                System.out.printf("Radius:%f, Area:%f, Circumference:%f%n", c.getRadius(), c.getArea(), c.getCircumference());
-            }
-            else if (o instanceof Complex c){
-                System.out.printf("||%s|| = %f%n", c.toString(), c.getLength());
-            }
+
+//            if (o instanceof String s) {
+//                String upper = s.toUpperCase();
+//                System.out.printf("Text:%s, Upper:%s%n", s, upper);
+//            }
+//            else if (o instanceof IntValue iVal) {
+//                int val = iVal.getValue();
+//                System.out.printf("%d * %d = %d%n", val, val, val * val);
+//            }
+//            else if (o instanceof Point p) {
+//                System.out.printf("Distance to origin:%f%n", p.euclideanDistance());
+//            }
+//            else if (o instanceof Circle c) {
+//                System.out.printf("Radius:%f, Area:%f, Circumference:%f%n", c.getRadius(), c.getArea(), c.getCircumference());
+//            }
+//            else if (o instanceof Complex c){
+//                System.out.printf("||%s|| = %f%n", c.toString(), c.getLength());
+//            }
             System.out.println("-------------------------------------------------------------------------------------");
         }
 
