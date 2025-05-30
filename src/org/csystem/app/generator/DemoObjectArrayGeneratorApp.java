64ff5b45dcb86251562a6fd9a40bcf88dd2a7aa4
m-gunes <mistafagunes@gmail.com>
2025-05-30 11:37:00 +0200

    instanceof pattern (pattern matching of instanceof)

--- src/org/csystem/app/generator/DemoObjectArrayGeneratorApp.java
+++ src/org/csystem/app/generator/DemoObjectArrayGeneratorApp.java
@@ -21,31 +21,21 @@ public class DemoObjectArrayGeneratorApp {
             System.out.println("-------------------------------------------------------------------------------------");
             System.out.printf("Dynamic type:%s%n", o.getClass().getName());
 
-            if (o instanceof String) {
-                String s = (String)o;
+            if (o instanceof String s) {
                 String upper = s.toUpperCase();
-
                 System.out.printf("Text:%s, Upper:%s%n", s, upper);
             }
-            else if (o instanceof IntValue) {
-                IntValue iVal = (IntValue)o;
+            else if (o instanceof IntValue iVal) {
                 int val = iVal.getValue();
-
                 System.out.printf("%d * %d = %d%n", val, val, val * val);
             }
-            else if (o instanceof Point) {
-                Point p = (Point)o;
-
+            else if (o instanceof Point p) {
                 System.out.printf("Distance to origin:%f%n", p.euclideanDistance());
             }
-            else if (o instanceof Circle) {
-                Circle c = (Circle)o;
-
+            else if (o instanceof Circle c) {
                 System.out.printf("Radius:%f, Area:%f, Circumference:%f%n", c.getRadius(), c.getArea(), c.getCircumference());
             }
-            else {
-                Complex c = (Complex)o;
-
+            else if (o instanceof Complex c){
                 System.out.printf("||%s|| = %f%n", c.toString(), c.getLength());
             }
             System.out.println("-------------------------------------------------------------------------------------");
