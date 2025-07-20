e07f44c4441f1c254a8954ebdd2b3c173503ae9d
m-gunes <mistafagunes@gmail.com>
2025-07-20 18:30:40 +0200

    ensureCapasity

--- src/org/csystem/app/App.java
+++ src/org/csystem/app/App.java
@@ -7,29 +7,14 @@ import java.util.*;
 class App {
     public static void main(String[] args)
     {
-        Vector texts = new Vector();
-        Random random = new Random();
-        Scanner kb = new Scanner(System.in);
-        while (true) {
-            System.out.print("Input a number:");
-            int n = Integer.parseInt(kb.nextLine());
-
-            if (n <= 0) break;
-
-            String text = StringUtil.generateRandomTextTR(random, n);
-            texts.add(text);
-        }
-
-        System.out.println();
+        Vector texts = new Vector(5);
         System.out.printf("Size:%d%n Capasity:%d%n", texts.size(), texts.capacity());
 
-        texts.trimToSize();
+        texts.ensureCapacity(7);
         System.out.printf("Size:%d%n Capasity:%d%n", texts.size(), texts.capacity());
 
-        texts.clear();
+        texts.ensureCapacity(23);
         System.out.printf("Size:%d%n Capasity:%d%n", texts.size(), texts.capacity());
 
-        texts.trimToSize();
-        System.out.printf("Size:%d%n Capasity:%d%n", texts.size(), texts.capacity());
     }
 }
