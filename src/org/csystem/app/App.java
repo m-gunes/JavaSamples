e9c0c625f7dc6e2e6c4029e5ebea63ad5106cdaa
Mustafa Gunes <mustafa.gunes@xparvision.com>
2025-08-07 18:28:57 +0200

    Random point generator

--- src/org/csystem/app/App.java
+++ src/org/csystem/app/App.java
@@ -1,20 +1,33 @@
 package org.csystem.app;
 
-import org.csystem.util.string.StringUtil;
-
-import java.util.*;
+import org.csystem.math.geometry.Circle;
+import org.csystem.math.geometry.Point;
 
 class App {
     public static void main(String[] args)
     {
-        Vector texts = new Vector(5);
-        System.out.printf("Size:%d%n Capasity:%d%n", texts.size(), texts.capacity());
+        int a = 10;
+        double b = 10.5;
+        boolean c = true;
+        Circle ci = new Circle(100.5);
+        Point p = Point.createCartesian(300, 67.8);
 
-        texts.ensureCapacity(7);
-        System.out.printf("Size:%d%n Capasity:%d%n", texts.size(), texts.capacity());
+        System.out.println("Circle -> " + ci);
+        System.out.println("Point -> " + p);
+        System.out.print(1);
+        System.out.printf("a = %s, b = %s, c = %s%n", Integer.valueOf(a), b, c);
+        System.out.println("a = " + a + ", b = " + b + ", c = " + c);
+        System.out.print("a:" + Integer.valueOf(a).toString());
 
-        texts.ensureCapacity(23);
-        System.out.printf("Size:%d%n Capasity:%d%n", texts.size(), texts.capacity());
+        System.out.println("-----");
 
+        Sample s = new Sample();
+        Sample s2= new Sample();
+        System.out.println(s);
+        System.out.println(s2);
     }
+}
+
+class Sample {
+
 }
