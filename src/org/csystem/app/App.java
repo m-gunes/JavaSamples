438584118ccc18688930fd9ff05cd6eb1bb2b761
Mustafa Gunes <mustafa.gunes@xparvision.com>
2025-08-23 15:33:55 +0200

    Exception handling

--- src/org/csystem/app/App.java
+++ src/org/csystem/app/App.java
@@ -2,37 +2,56 @@ package org.csystem.app;
 
 import org.csystem.util.console.Console;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 class App {
     public static void main(String[] args)
     {
-        Sample s1 = new Sample(3);
-        Console.writeLine("-----------------");
+        try {
+            Scanner kb = new Scanner(System.in);
+            System.out.print("input a number:");
+            double a = kb.nextInt();
+            double result;
+            result = MathUtil.log10(a);
+            System.out.printf("log10(%f) = %f%n", a, result);
+        }
+        catch (MathException ignore) {
+            System.out.println("Invalid input for logarithm");
+        }
+//        catch (NegativeException ignore) {
+//            System.out.println("Negative value is invalid for logarithm");
+//        }
+//        catch (ZeroException ignore) {
+//            System.out.println("Zero value is invalid for logarithm");
+//        }
+        catch (InputMismatchException ignore) {
+            System.out.println("Invalid number");
+        }
     }
 }
 
-class Sample {
-
-   {
-        Console.writeLine("Non-Static initializer 1");
-   }
-
-   public Sample()
-   {
-       Console.writeLine("I'm a default Ctor");
-   }
-
-    public Sample(int a)
+class MathUtil {
+    public static double log10(double a)
     {
-        this();
-        Console.writeLine("I'm a Ctor with parameter int");
-    }
+        if (a < 0)
+            throw new NegativeException();
 
-    {
-        Console.writeLine("Non-Static initializer 2");
-    }
+        if (a == 0)
+            throw new ZeroException();
 
-    {
-        Console.writeLine("Non-Static initializer 3");
+        return Math.log10(a);
     }
+}
+
+class NegativeException extends MathException {
+
+}
+
+class ZeroException extends MathException {
+
+}
+
+class MathException extends RuntimeException {
 
 }
