d52ac1dc886300d734cda0001764655e16768920
Mustafa Gunes <mustafa.gunes@xparvision.com>
2025-09-14 10:49:05 +0200

    checked exception wrapper exception solution

--- src/org/csystem/app/App.java
+++ src/org/csystem/app/App.java
@@ -2,56 +2,70 @@ package org.csystem.app;
 
 import org.csystem.util.console.Console;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 class App {
     public static void main(String[] args)
     {
-        try {
-            Util.doWork();
-        }
-        catch (Exception ex) {
-            Console.writeLine("Math Exception:%s", ex.getMessage());
-        }
-        finally {
-            System.out.println("Finally block in main");
-        }
-
-        System.out.println("main ends..");
     }
 }
 
-class Util {
-    public static void doWork()
-    {
-        try {
-            Scanner kb = new Scanner(System.in);
-            double a = Console.readDouble("Input a number:", "Invalid value!...");
-            double result;
-            result = MathUtil.log10(a);
-            System.out.printf("log10(%f) = %f%n", a, result);
-        }
-        catch (MathException ex) {
-            Console.writeLine("Math Exception: %s", ex.getMessage());
-        }
-        finally {
-            System.out.println("finally block in doWork");
-        }
-
-        System.out.println("doWork ends!...");
+class Sample {
+    public static void doWork() throws IOException {
+        //..
     }
 }
 
-class MathUtil {
-    public static double log10(double a) throws MathException
+class E extends A {
+    public void foo() throws MathException {
+        try {
+            Sample.doWork();
+        } catch (IOException ex) {
+            throw new WrapperException("io problem occurred", ex);
+        }
+    }
+}
+
+class D extends A {
+    public void foo() throws MathException {
+    }
+}
+class C extends A {
+    public void foo() throws NegativeInfinityException, NaNException {
+    }
+}
+
+class B extends A {
+    public void foo() {
+        //..
+    }
+}
+
+abstract class A {
+    public abstract void foo() throws MathException;
+}
+
+class WrapperException extends RuntimeException {
+    public WrapperException()
     {
-        if (a < 0)
-            throw new NegativeInfinityException("Value %f can not be negative".formatted(a));
 
-        if (a == 0)
-            throw new NaNException("Value can not be zero");
+    }
 
-        return Math.log10(a);
+    public WrapperException(String message)
+    {
+        this(message, null);
+    }
+
+    public WrapperException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public String getMessage()
+    {
+        Throwable cause = getCause();
+        return "Message: %s%s".formatted(super.getMessage(), cause != null ? "Cause Message: %s".formatted(cause.getMessage()): "");
     }
 }
 
