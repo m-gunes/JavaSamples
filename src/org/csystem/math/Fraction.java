02024912004e5dd2ad7bf1da2b16b278d8e57911
m-gunes <mistafagunes@gmail.com>
2025-09-16 23:15:50 +0200

    Fraction WIP

--- src/org/csystem/math/Fraction.java
+++ src/org/csystem/math/Fraction.java
@@ -133,37 +133,33 @@ public class Fraction {
 */
 
 
-/**
- * Fraction class that represents fraction
- * Last Update: 28th March 2025
- * @author Java-Jan-2024 Group
- */
-
-/*
->**Açıklamalar:**
-//>- Sınıf Matematikteki bir kesri temsil ettiğinden pay (numerator) ve payda (denominator) değerleri tutulacaktır.
-//>- Sınıfın ilgili set ve get metotları yazılacaktır.
-//        >- Pay'ın sıfırdan farklı veya sıfır ve paydanın sıfır olması durumunda uygun mesajla birlikte program sonlandırılacaktır.
-//        >- Kesir her durumda sadeleşmiş bir biçimde tutulacaktır. Örneğin kesrin pay ve paydası sırasıyla 4 ve 18 olarak verildiğinde kesir 2 / 9 olarak tutulacaktır.
-//        >- Kesir negatif ise işaret payda bulunacaktır. Örneğin kesrin pay ve paydası sırasıyla 3 ve -4 olarak verilmişse  kesir -3 / 4 biçiminde tutulacaktır.
-//        >- Kesrin pay ve paydasının her ikisinin birden negatif olması durumunda kesir pozitif olarak tutulacaktır.
-//        >- Kesrin payının sıfır olması durumunda payda ne olursa olsun 1(bir) yapılacaktır.
->- Sınıfın iki kesri toplayan, bir kesir ile bir tamsayıyı toplayan metotları olacaktır. Aynı işlemler çıkarma, çarpma ve bölme için de yapılacaktır.
-        >- Sınıfın kesri 1(bir) artıran ve bir azaltan inc ve dec metotları yazılacaktır.
->- Sınıfın toString metodu şu formatta yazı döndürecek şekilde override edilecektir. Örneğin 3 / 10 kesri için -> 3 / 10 = 3.333333    10 / 1 kesri için -> 10 Ondalık kısımda 6 basamak gösterilecektir. Geri kalan basamaklar yuvarlanacaktır.
-        >- Sınıfın equals metodu iki kesrin eşitlik karşılaştırması için override edilecektir.
-        >- Sınıfın default ctor'u "0 / 1" kesrini temsil eden nesneyi yaratmak için kullanılabilecektir. Sınıfın compareTo metodu iki kesrin büyüklük küçüklük karşılaştırmasını yapacaktır. String sınıfının compareTo metodunun mantığına göre tasarlayınız.
-        >- Kesrin double türden ondalık değerini döndüren getRealValue metodu yazılacaktır.
-        >- Sınıfın public bölümünü değiştirmeden istediğiniz değişikliği ve eklemeleri yapabilirsiniz.
->- Sınıfın public bölümü ile birlikte iskeleti şu şekildedir:
- */
-
 package org.csystem.math;
 
 
 public class Fraction {
     private int m_a; // numerator
     private int m_b; // denominator
+
+
+    private static Fraction add(int a1, int b1, int a2, int b2)
+    {
+        return new Fraction(a1 * b2 + a2 * b1, b1 * b2);
+    }
+
+    private static Fraction subtract(int a1, int b1, int a2, int b2)
+    {
+        return add(a1, b1, -a2, b2);
+    }
+
+    private static Fraction multiply(int a1, int b1, int a2, int b2)
+    {
+        return new Fraction(a1 * a2, b1 * b2);
+    }
+
+    private static Fraction divide(int a1, int b1, int a2, int b2)
+    {
+        return multiply(a1, b1, b2, a2);
+    }
 
     private static void check(int a, int b)
     {
@@ -201,8 +197,7 @@ public class Fraction {
     private void set(int a, int b)
     {
        if (a == 0) {
-           m_a = 0;
-           m_b = 1;
+           setFields(0,1);
            return;
        }
        setFields(a, b);
@@ -258,72 +253,62 @@ public class Fraction {
 
     public Fraction add(Fraction other)
     {
-        int a1 = other.getNumerator();
-        int b1 = other.getDenominator();
-        int a2 = this.getNumerator();
-        int b2 = this.getDenominator();
-
-        if (b1 == b2)
-            return new Fraction(a1 + a2, b1);
-
-
-        throw new UnsupportedOperationException("Todo");
+        return add(m_a, m_b, other.m_a, other.m_b);
     }
 
     public Fraction add(int val)
     {
-        // check the rules
-        throw new UnsupportedOperationException("TODO:");
+        return add(m_a, m_b, val, 1);
     }
 
     public Fraction subtract(Fraction other)
     {
-        throw new UnsupportedOperationException("TODO:");
+        return subtract(m_a, m_b, other.m_a, other.m_b);
     }
 
     public Fraction subtract(int val)
     {
-        throw new UnsupportedOperationException("TODO:");
+        return subtract(m_a, m_b, val, 1);
     }
 
     public Fraction multiply(Fraction other)
     {
-        throw new UnsupportedOperationException("TODO:");
+        return multiply(m_a, m_b, other.m_a, other.m_b);
     }
 
     public Fraction multiply(int val)
     {
-        throw new UnsupportedOperationException("TODO:");
+        return multiply(m_a, m_b, val, 1);
     }
 
     public Fraction divide(Fraction other)
     {
-        throw new UnsupportedOperationException("TODO:");
+        return divide(m_a, m_b, other.m_a, other.m_b);
     }
 
     public Fraction divide(int val)
     {
-        throw new UnsupportedOperationException("TODO:");
+        return divide(m_a, m_b, val, 1);
     }
 
     public void inc()
     {
-        throw new UnsupportedOperationException("TODO:");
+        m_a += m_b;
     }
 
     public void dec()
     {
-        throw new UnsupportedOperationException("TODO:");
+        m_a -= m_b;
     }
 
     public int compareTo(Fraction other)
     {
-        throw new UnsupportedOperationException("TODO:");
+        return m_a * other.m_b - other.m_a * m_b;
     }
 
-    public boolean equals()
+    public boolean equals(Object other)
     {
-        throw new UnsupportedOperationException("TODO:");
+        return other instanceof Fraction f && compareTo(f) == 0;
     }
 
     public String toString()
