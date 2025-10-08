ea8093f43976317b8680b104bd87b8c68510ac0b
m-gunes <mistafagunes@gmail.com>
2025-10-08 20:16:34 +0200

    interview question

---
+++ src/org/csystem/subject/generics/question/App.java
@@ -0,0 +1,25 @@
+package org.csystem.subject.generics.question;
+
+public class App {
+    public static void main(String[] args)
+    {
+        Sample.a = 10;
+        Sample.a = "Ali";
+    }
+}
+
+class Sample<T> {
+    public static T a; // static context te T yi yakalamanin imkani yok. Static ler scopa dahil degil, generic tur parametreleri icin
+    // Cevap: Aşağıdaki demo örnekte static bir veri elemanı, ait olduğu generic sınıfın generic tür parametresi türünden olamayacağından error oluşur
+    // Bir sinifin geric turden `static` bir elemani olamiyor. methot gibi generic yapilamiyor.
+}
+
+
+
+/*
+* 1. Error olusur
+* 2. Derlenir ekrana Ali yazisi basilir
+* 3. Derlenir ekrana 10 yazisi basilir
+* 4. Hicbiri
+* */
+
