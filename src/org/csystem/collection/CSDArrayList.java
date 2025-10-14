93cf68c38432188f2456b47d4f8d759a5e1e7b4a
m-gunes <mistafagunes@gmail.com>
2025-10-14 08:05:49 +0200

    homework - CSDArrayList<E>

---
+++ src/org/csystem/collection/CSDArrayList.java
@@ -0,0 +1,79 @@
+package org.csystem.collection;
+
+/**
+ * Sınıf Çalışması: İskeleti verilen CSDArrayList<E> sınıfını aşağıdaki açıklamalara göre yazınız:<p>
+ * Açıklamalar: <p>
+ * - Sınıfın metotları ArrayList<E> sınıfındaki metotların yaptığı işleri yapacaktır. <p>
+ * - Metotların karmaşıklıklarının ArrayList<E> ile aynı olması gerekir. <p>
+ * - Metotlarda fırlatılacak exception'lar yine ArrayList<E> ile aynı olacaktır. <p>
+ * - Sınıfın public bölümünü değiştirmeden istediğiniz eklemeyi yapabilirsiniz. <p>
+ * - CSDArrayList<E> sınıfının iskeleti şu şekildedir:
+ */
+
+public class CSDArrayList<E> {
+
+    public CSDArrayList()
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public CSDArrayList(int initialCapacity)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public boolean add(E element)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void add(int index, E element)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public int capacity()
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void clear()
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void ensureCapacity(int minCapacity)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public E get(int index)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public E remove(int index)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public E set(int index, E element)
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public int size()
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void trimToSize()
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public String toString()
+    {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
