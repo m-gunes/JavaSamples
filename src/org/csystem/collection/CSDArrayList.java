352d073281917f68a3a9a44af32d75913d84d410
Mustafa Gunes <mustafa.gunes@xparvision.com>
2025-10-15 19:02:15 +0200

    CSDArrayList is done for now

--- src/org/csystem/collection/CSDArrayList.java
+++ src/org/csystem/collection/CSDArrayList.java
@@ -1,5 +1,7 @@
 package org.csystem.collection;
 
+import java.util.Arrays;
+
 /**
  * Sınıf Çalışması: İskeleti verilen CSDArrayList<E> sınıfını aşağıdaki açıklamalara göre yazınız:<p>
  * Açıklamalar: <p>
@@ -15,15 +17,37 @@ public class CSDArrayList<E> {
     private E [] m_elements;
     private int m_index;
 
-    private void IllegalArgumentException(String message)
+    private void throwIllegalArgumentException(String message)
     {
         throw new IllegalArgumentException(message);
+    }
+
+    private void throwIndexOutOfBoundsException(String message)
+    {
+        throw new IndexOutOfBoundsException(message);
     }
 
     private void checkCapacity(int capacity)
     {
         if (capacity < 0)
-            IllegalArgumentException(String.format("Capacity must be non-negative:%d", capacity));
+            throwIllegalArgumentException(String.format("Capacity must be non-negative:%d", capacity));
+    }
+
+    private void changeCapacity(int capacity)
+    {
+        m_elements = Arrays.copyOf(m_elements, capacity);
+    }
+
+    private void checkIndex(int index)
+    {
+        if (index < 0 || index > m_index)
+            throwIndexOutOfBoundsException(String.format("Index out of bounds:%d", index));
+    }
+
+    private void increaseCapacityIfNecessary()
+    {
+        if (m_elements.length == m_index)
+            changeCapacity(m_elements.length == 0 ? 1 : m_elements.length * 2);
     }
 
     public CSDArrayList()
@@ -39,56 +63,84 @@ public class CSDArrayList<E> {
 
     public boolean add(E element)
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        increaseCapacityIfNecessary();
+        m_elements[m_index++] = element;
+        return true;
     }
 
     public void add(int index, E element)
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        increaseCapacityIfNecessary();
+
+        for (int i = m_index++; i > index; --i)
+            m_elements[i] = m_elements[i - 1];
+
+        m_elements[index] = element;
     }
 
     public int capacity()
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return m_elements.length;
     }
 
     public void clear()
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        for (int i = 0; i < m_index; ++i)
+            m_elements[i] = null;
+
+        m_index = 0;
     }
 
     public void ensureCapacity(int minCapacity)
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (minCapacity > m_elements.length)
+            changeCapacity(Math.max(minCapacity, m_elements.length * 2));
     }
 
     public E get(int index)
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        checkIndex(index);
+        return m_elements[index];
     }
 
     public E remove(int index)
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        checkIndex(index);
+        E oldElement = m_elements[index];
+
+        for (int i = index; i < m_index - 1; ++i)
+            m_elements[i] = m_elements[i+1];
+
+        m_elements[--m_index] = null;
+
+        return oldElement;
     }
 
     public E set(int index, E element)
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        checkIndex(index);
+        E oldElement = m_elements[index];
+        m_elements[index] = element;
+        return oldElement;
     }
 
     public int size()
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return m_index;
     }
 
     public void trimToSize()
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (m_elements.length != m_index)
+            changeCapacity(m_index);
     }
 
     public String toString()
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < m_index; ++i)
+            sb.append(m_elements[i]).append(", ");
+
+        return (m_index != 0 ? sb.substring(0, sb.length() -2) : sb.toString()) + "]";
     }
 }
