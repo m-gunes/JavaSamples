4c666cf9887a8bc8779ea984f339645d0381b0d3
m-gunes <mistafagunes@gmail.com>
2026-09-16 11:21:58 +0200

    Backup and copy files via prompt

--- src/org/csystem/app/io/file/copy/BackupViaPrompt.java
+++ src/org/csystem/app/io/file/copy/BackupViaPrompt.java
@@ -1,14 +1,32 @@
 package org.csystem.app.io.file.copy;
 
 import org.csystem.function.IPredicate;
+import org.csystem.util.console.Console;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class BackupViaPrompt implements IPredicate<Path> {
+    private char getOption()
+    {
+        char c;
+        do
+            c = Console.readChar("Backup file exists. Do you want to overwrite?", "Invalid char");
+        while (c != 'y' && c != 'Y' && c != 'n' && c != 'N');
+
+        return c;
+    }
+
     @Override
     public boolean test(Path path) throws IOException
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        char option = getOption();
+
+        if (option == 'y' || option == 'Y')
+            Files.copy(path, Path.of("%s-bak".formatted(path.toString())), StandardCopyOption.REPLACE_EXISTING);
+
+        return true;
     }
 }
