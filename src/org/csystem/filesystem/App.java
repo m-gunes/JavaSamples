eb1247c0b19832453ba0cf171df021e884234887
m-gunes <mistafagunes@gmail.com>
2026-09-16 12:18:49 +0200

    Files.createDirectory and Files.createDirectories

--- src/org/csystem/filesystem/App.java
+++ src/org/csystem/filesystem/App.java
@@ -3,6 +3,6 @@ package org.csystem.filesystem;
 public class App {
     public static void main(String[] args)
     {
-        CopyLes6.run(args);
+        Lesson8.run(args);
     }
 }
---
+++ src/org/csystem/filesystem/Lesson8.java
@@ -0,0 +1,36 @@
+package org.csystem.filesystem;
+
+import org.csystem.util.console.Console;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.csystem.util.console.CommandLineArgsUtil.checkLengthEquals;
+
+public class Lesson8 {
+
+    public static void run(String[] args)
+    {
+        checkLengthEquals(1, args.length, "Wrong number of arguments");
+
+        try {
+            Path path = Path.of(args[0]);
+
+//            Files.createDirectory(path);
+            Files.createDirectories(path);
+            Console.writeLine("Directory '%s' created", args[0]);
+        }
+        catch (FileAlreadyExistsException e) {
+            Console.writeErrLine("'%s' exists", e.getFile());
+        }
+        catch (IOException e) {
+            Console.writeErrLine("IO error occurred:%s", e.getMessage());
+        }
+        catch (Exception e) {
+            Console.writeErrLine("Error occurred:%s", e.getMessage());
+        }
+    }
+}
+
